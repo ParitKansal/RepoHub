e594a06d10ab6fcd777af0207f74b342fba8c1f6
ParitKansalXelpmoc <parit.kansal@xelpmoc.in>
2026-06-29 19:17:13 +0530

    Phase 4: Collaboration (Issues)

--- models.py
+++ models.py
@@ -19,5 +19,19 @@ class Repository(Base):
     name = Column(String, index=True)
     description = Column(Text, nullable=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
-
+    
     owner = relationship("User", back_populates="repositories")
+    issues = relationship("Issue", back_populates="repository")
+
+class Issue(Base):
+    __tablename__ = "issues"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(Text, nullable=True)
+    status = Column(String, default="Open")
+    repo_id = Column(Integer, ForeignKey("repositories.id"))
+    author_id = Column(Integer, ForeignKey("users.id"))
+    
+    repository = relationship("Repository", back_populates="issues")
+    author = relationship("User")
